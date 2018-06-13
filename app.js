14fe583bab99ca46a16c8ec5f33ede74f9387240
SIMOCCAuch19 <clerc6nathan@hotmail.fr>
2018-06-13 11:26:02 +0200

    selecteur de nom fini

---
+++ app.js
@@ -0,0 +1,7 @@
+function select(){
+var array=['Dylan','Océane', 'Orélien','Benji', 'Helder', 'Luc', 'Marc', 'Adeline', 'Nicolas', 'Dania', 'Max', 'Jonathan', 'Zakia', 'Marco', 'Alex', 'Houssine', 'Nathan'];
+var min = 0;
+var max=16;
+var index =Math.floor( min + (max-min+1)*Math.random());
+var select= alert(array[index]);
+}
